8b1f9e15d1ea6be3db8e9440a8f5d81fc59fae8b
Egor Prokofev <egor.prokofev.ru@gmail.com>
2023-03-15 15:01:48 +0300

    Added Ladder Actor

---
+++ Source/AcidHouse/Actors/Interactive/Enviroment/Ladder.cpp
@@ -0,0 +1,79 @@
+#include "Ladder.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SceneComponent.h"
+#include "Components/InstancedStaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "../../../AcidHouseTypes.h"
+
+ALadder::ALadder()
+{
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("LadderRoot"));
+
+	LeftRailMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LeftRail"));
+	LeftRailMeshComponent->SetupAttachment(RootComponent);
+
+	RightRailMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RightRail"));
+	RightRailMeshComponent->SetupAttachment(RootComponent);
+
+	StepsMeshComponent = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Steps"));
+	StepsMeshComponent->SetupAttachment(RootComponent);
+
+	InteractionVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("InteractionVolume"));
+	InteractionVolume->SetupAttachment(RootComponent);
+}
+
+void ALadder::OnConstruction(const FTransform& Transform)
+{
+	LeftRailMeshComponent->SetRelativeLocation(FVector(0.0f, -LadderWidth * 0.5f, LadderHeight * 0.5f));
+	RightRailMeshComponent->SetRelativeLocation(FVector(0.0f, LadderWidth * 0.5f, LadderHeight * 0.5f));
+
+	UStaticMesh* LeftRailMesh = LeftRailMeshComponent->GetStaticMesh();
+	if (IsValid(LeftRailMesh))
+	{
+		float MeshHeight = LeftRailMesh->GetBoundingBox().GetSize().Z;
+		if (!FMath::IsNearlyZero(MeshHeight))
+		{
+			LeftRailMeshComponent->SetRelativeScale3D(FVector(1.0f, 1.0f, LadderHeight / MeshHeight));
+		}
+	}
+
+	UStaticMesh* RightRailMesh = RightRailMeshComponent->GetStaticMesh();
+	if (IsValid(RightRailMesh))
+	{
+		float MeshHeight = RightRailMesh->GetBoundingBox().GetSize().Z;
+		if (!FMath::IsNearlyZero(MeshHeight))
+		{
+			RightRailMeshComponent->SetRelativeScale3D(FVector(1.0f, 1.0f, LadderHeight / MeshHeight));
+		}
+	}
+
+	UStaticMesh* StepsMesh = StepsMeshComponent->GetStaticMesh();
+	if (IsValid(StepsMesh))
+	{
+		float MeshWidth = StepsMesh->GetBoundingBox().GetSize().Y;
+		if (!FMath::IsNearlyZero(MeshWidth))
+		{
+			StepsMeshComponent->SetRelativeScale3D(FVector(1.0f, LadderWidth / MeshWidth, 1.0f));
+		}
+	}
+
+	StepsMeshComponent->ClearInstances();
+
+	uint32 StepsCount = FMath::FloorToInt((LadderHeight - BottomStepOffset) / StepsInterval);
+	for (uint32 i = 0; i < StepsCount; ++i)
+	{
+		FTransform InstanceTransform(FVector(1.0f, 1.0f, BottomStepOffset + i*StepsInterval));
+		StepsMeshComponent->AddInstance(InstanceTransform);
+	}
+
+	float BoxDepthExtent = GetLadderInteractionBox()->GetUnscaledBoxExtent().X;
+	GetLadderInteractionBox()->SetBoxExtent(FVector(BoxDepthExtent, LadderWidth * 0.5f, LadderHeight * 0.5f));
+	GetLadderInteractionBox()->SetRelativeLocation(FVector(BoxDepthExtent, 0.0f, LadderHeight * 0.5f));
+	InteractionVolume->SetCollisionProfileName(CollisionProfilePawnInteractionVolume);
+	InteractionVolume->SetGenerateOverlapEvents(true);
+}
+
+class UBoxComponent* ALadder::GetLadderInteractionBox() const
+{
+	return StaticCast<UBoxComponent*>(InteractionVolume);
+}
---
+++ Source/AcidHouse/Actors/Interactive/Enviroment/Ladder.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../InteractiveActor.h"
+#include "Ladder.generated.h"
+
+class UStaticMeshComponent;
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class ACIDHOUSE_API ALadder : public AInteractiveActor
+{
+	GENERATED_BODY()
+
+public:
+	ALadder();
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ladder parametrs", meta = (ClampMin = 0, UIMin = 0))
+	float LadderHeight = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ladder parametrs", meta = (ClampMin = 0, UIMin = 0))
+	float LadderWidth = 50.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ladder parametrs", meta = (ClampMin = 0, UIMin = 0))
+	float StepsInterval = 25.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ladder parametrs", meta = (ClampMin = 0, UIMin = 0))
+	float BottomStepOffset = 25.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* RightRailMeshComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* LeftRailMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UInstancedStaticMeshComponent* StepsMeshComponent; 
+
+	class UBoxComponent* GetLadderInteractionBox() const;
+
+};
