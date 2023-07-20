4e994053bde9e3b166ecf4abb52f491b09bcc054
Egor Prokofev <egor.prokofev.ru@gmail.com>
2023-07-20 20:25:21 +0300

    Added creating, finding, joining, destroying session

--- Source/AcidHouse/AHGameInstance.cpp
+++ Source/AcidHouse/AHGameInstance.cpp
@@ -1,4 +1,26 @@
 #include "AHGameInstance.h"
+#include "Kismet/GameplayStatics.h"
+#include "Net/UnrealNetwork.h"
+#include "OnlineSubsystem.h"
+#include "OnlineSubsystemUtils.h"
+#include "OnlineSubsystemTypes.h"
+
+
+UAHGameInstance::UAHGameInstance()
+{
+	OnCreateSessionCompleteDelegate = FOnCreateSessionCompleteDelegate::CreateUObject(this, &UAHGameInstance::OnCreateSessionComplete);
+	OnStartSessionCompleteDelegate = FOnStartSessionCompleteDelegate::CreateUObject(this, &UAHGameInstance::OnStartOnlineGameComplete);
+	OnFindSessionsCompleteDelegate = FOnFindSessionsCompleteDelegate::CreateUObject(this, &UAHGameInstance::OnFindSessionsComplete);
+	OnJoinSessionCompleteDelegate = FOnJoinSessionCompleteDelegate::CreateUObject(this, &UAHGameInstance::OnJoinSessionComplete);
+	OnDestroySessionCompleteDelegate = FOnDestroySessionCompleteDelegate::CreateUObject(this, &UAHGameInstance::OnDestroySessionComplete);
+}
+
+void UAHGameInstance::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(UAHGameInstance, ServerName);
+	DOREPLIFETIME(UAHGameInstance, MaxPlayers);
+}
 
 bool UAHGameInstance::ProcessConsoleExec(const TCHAR* Cmd, FOutputDevice& Ar, UObject* Executor)
 {
@@ -14,4 +36,312 @@ bool UAHGameInstance::ProcessConsoleExec(const TCHAR* Cmd, FOutputDevice& Ar, UO
 	}
 
 	return bResult;
+}
+
+void UAHGameInstance::Init()
+{
+	Super::Init();
+	OnNetworkFailureEventHandle = GEngine->OnNetworkFailure().AddUObject(this, &UAHGameInstance::HandleNetworkFailure);
+	OnTravelFailureEventHandle = GEngine->TravelFailureEvent.AddUFunction(this, "OnTravelFailure");
+}
+
+void UAHGameInstance::Shutdown()
+{
+	GEngine->OnNetworkFailure().Remove(OnNetworkFailureEventHandle);
+	GEngine->TravelFailureEvent.Remove(OnTravelFailureEventHandle);
+	Super::Shutdown();
+}
+
+bool UAHGameInstance::HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLAN, bool bIsPresence, int32 MaxNumPlayers)
+{
+	// Get the Online Subsystem to work with
+	IOnlineSubsystem* const OnlineSub = IOnlineSubsystem::Get();
+
+	if (OnlineSub)
+	{
+		// Get the Session Interface, so we can call the "CreateSession" function on it
+		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+
+		if (Sessions.IsValid() && UserId.IsValid())
+		{
+			/*
+				Fill in all the Session Settings that we want to use.
+
+				There are more with SessionSettings.Set(...);
+				For example the Map or the GameMode/Type.
+			*/
+			SessionSettings = MakeShareable(new FOnlineSessionSettings());
+
+			SessionSettings->bIsLANMatch = bIsLAN;
+			SessionSettings->bUsesPresence = bIsPresence;
+			SessionSettings->NumPublicConnections = MaxNumPlayers;
+			SessionSettings->NumPrivateConnections = 0;
+			SessionSettings->bAllowInvites = true;
+			SessionSettings->bAllowJoinInProgress = true;
+			SessionSettings->bShouldAdvertise = true;
+			SessionSettings->bAllowJoinViaPresence = true;
+			SessionSettings->bAllowJoinViaPresenceFriendsOnly = false;
+
+			SessionSettings->Set(SETTING_MAPNAME, LobbyMapName.ToString(), EOnlineDataAdvertisementType::ViaOnlineService);
+
+			// Set the delegate to the Handle of the SessionInterface
+			OnCreateSessionCompleteDelegateHandle = Sessions->AddOnCreateSessionCompleteDelegate_Handle(OnCreateSessionCompleteDelegate);
+
+			// Our delegate should get called when this is complete (doesn't need to be successful!)
+			return Sessions->CreateSession(*UserId, SessionName, *SessionSettings);
+		}
+	}
+	else
+	{
+		DisplayNetworkErrorMessage("No OnlineSubsytem found!");
+	}
+
+	return false;
+}
+
+void UAHGameInstance::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("OnCreateSessionComplete %s, %d"), *SessionName.ToString(), bWasSuccessful));
+
+	if (!bWasSuccessful)
+	{
+		DisplayNetworkErrorMessage("Failed to create session! Please try again");
+		return;
+	}
+
+	// Get the OnlineSubsystem so we can get the Session Interface
+	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get();
+	if (OnlineSub)
+	{
+		// Get the Session Interface to call the StartSession function
+		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+
+		if (Sessions.IsValid())
+		{
+			// Clear the SessionComplete delegate handle, since we finished this call
+			Sessions->ClearOnCreateSessionCompleteDelegate_Handle(OnCreateSessionCompleteDelegateHandle);
+			if (bWasSuccessful)
+			{
+				// Set the StartSession delegate handle
+				OnStartSessionCompleteDelegateHandle = Sessions->AddOnStartSessionCompleteDelegate_Handle(OnStartSessionCompleteDelegate);
+
+				// Our StartSessionComplete delegate should get called after this
+				Sessions->StartSession(SessionName);
+			}
+		}
+	}
+}
+
+void UAHGameInstance::OnStartOnlineGameComplete(FName SessionName, bool bWasSuccessful)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("OnStartSessionComplete %s, %d"), *SessionName.ToString(), bWasSuccessful));
+
+	if (!bWasSuccessful)
+	{
+		DisplayNetworkErrorMessage(TEXT("OnStartSessionCompletCannot start online game! Please try again"));
+		return;
+	}
+
+	// Get the Online Subsystem so we can get the Session Interface
+	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get();
+	if (OnlineSub)
+	{
+		// Get the Session Interface to clear the Delegate
+		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+		if (Sessions.IsValid())
+		{
+			// Clear the delegate, since we are done with this call
+			Sessions->ClearOnStartSessionCompleteDelegate_Handle(OnStartSessionCompleteDelegateHandle);
+		}
+	}
+
+	// If the start was successful, we can open a NewMap if we want. Make sure to use "listen" as a parameter!
+	UGameplayStatics::OpenLevel(GetWorld(), LobbyMapName, true, "listen");
+}
+
+void UAHGameInstance::FindSessions(TSharedPtr<const FUniqueNetId> UserId, bool bIsLAN, bool bIsPresence)
+{
+	// Get the OnlineSubsystem we want to work with
+	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get();
+
+	if (OnlineSub)
+	{
+		// Get the SessionInterface from our OnlineSubsystem
+		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+
+		if (Sessions.IsValid() && UserId.IsValid())
+		{
+			/*
+				Fill in all the SearchSettings, like if we are searching for a LAN game and how many results we want to have!
+			*/
+			SessionSearch = MakeShareable(new FOnlineSessionSearch());
+
+			SessionSearch->bIsLanQuery = bIsLAN;
+			SessionSearch->MaxSearchResults = 20;
+			SessionSearch->PingBucketSize = 50;
+
+			// We only want to set this Query Setting if "bIsPresence" is true
+			if (bIsPresence)
+			{
+				SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, bIsPresence, EOnlineComparisonOp::Equals);
+			}
+
+			TSharedRef<FOnlineSessionSearch> SearchSettingsRef = SessionSearch.ToSharedRef();
+
+			// Set the Delegate to the Delegate Handle of the FindSession function
+			OnFindSessionsCompleteDelegateHandle = Sessions->AddOnFindSessionsCompleteDelegate_Handle(OnFindSessionsCompleteDelegate);
+
+			// Finally call the SessionInterface function. The Delegate gets called once this is finished
+			Sessions->FindSessions(*UserId, SearchSettingsRef);
+		}
+	}
+	else
+	{
+		// If something goes wrong, just call the Delegate Function directly with "false".
+		OnFindSessionsComplete(false);
+	}
+}
+
+void UAHGameInstance::OnFindSessionsComplete(bool bWasSuccessful)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("OFindSessionsComplete bSuccess: %d"), bWasSuccessful));
+
+	bool bIsMatchfound = false;
+	// Get OnlineSubsystem we want to work with
+	IOnlineSubsystem* const OnlineSub = IOnlineSubsystem::Get();
+	if (OnlineSub)
+	{
+		// Get SessionInterface of the OnlineSubsystem
+		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+		if (Sessions.IsValid())
+		{
+			// Clear the Delegate handle, since we finished this call
+			Sessions->ClearOnFindSessionsCompleteDelegate_Handle(OnFindSessionsCompleteDelegateHandle);
+
+			// Just debugging the Number of Search results. Can be displayed in UMG or something later on
+			GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("Num Search Results: %d"), SessionSearch->SearchResults.Num()));
+
+			// If we have found at least 1 session, we just going to debug them. You could add them to a list of UMG Widgets, like it is done in the BP version!
+			if (SessionSearch->SearchResults.Num() > 0)
+			{
+				// "SessionSearch->SearchResults" is an Array that contains all the information. You can access the Session in this and get a lot of information.
+				// This can be customized later on with your own classes to add more information that can be set and displayed
+				bIsMatchfound = true;
+				for (int32 SearchIdx = 0; SearchIdx < SessionSearch->SearchResults.Num(); SearchIdx++)
+				{
+					// OwningUserName is just the SessionName for now. I guess you can create your own Host Settings class and GameSession Class and add a proper GameServer Name here.
+					// This is something you can't do in Blueprint for example!
+					GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("Session Number: %d | Sessionname: %s "), SearchIdx + 1, *(SessionSearch->SearchResults[SearchIdx].Session.OwningUserName)));
+				}
+			}
+		}
+	}
+
+	if (OnMatchFound.IsBound())
+	{
+		OnMatchFound.Broadcast(bIsMatchfound);
+	}
+}
+
+bool UAHGameInstance::JoinFoundOnlineSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, const FOnlineSessionSearchResult& SearchResult)
+{
+	// Return bool
+	bool bSuccessful = false;
+
+	// Get OnlineSubsystem we want to work with
+	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get();
+
+	if (OnlineSub)
+	{
+		// Get SessionInterface from the OnlineSubsystem
+		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+
+		if (Sessions.IsValid() && UserId.IsValid())
+		{
+			// Set the Handle again
+			OnJoinSessionCompleteDelegateHandle = Sessions->AddOnJoinSessionCompleteDelegate_Handle(OnJoinSessionCompleteDelegate);
+
+			// Call the "JoinSession" Function with the passed "SearchResult". The "SessionSearch->SearchResults" can be used to get such a
+			// "FOnlineSessionSearchResult" and pass it. Pretty straight forward!
+			bSuccessful = Sessions->JoinSession(*UserId, SessionName, SearchResult);
+		}
+	}
+
+	return bSuccessful;
+}
+
+void UAHGameInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("OnJoinSessionComplete %s, %d"), *SessionName.ToString(), static_cast<int32>(Result)));
+
+	// Get the OnlineSubsystem we want to work with
+	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get();
+	if (OnlineSub)
+	{
+		// Get SessionInterface from the OnlineSubsystem
+		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+
+		if (Sessions.IsValid())
+		{
+			// Clear the Delegate again
+			Sessions->ClearOnJoinSessionCompleteDelegate_Handle(OnJoinSessionCompleteDelegateHandle);
+
+			// Get the first local PlayerController, so we can call "ClientTravel" to get to the Server Map
+			// This is something the Blueprint Node "Join Session" does automatically!
+			APlayerController* const PlayerController = GetFirstLocalPlayerController();
+
+			// We need a FString to use ClientTravel and we can let the SessionInterface contruct such a
+			// String for us by giving him the SessionName and an empty String. We want to do this, because
+			// Every OnlineSubsystem uses different TravelURLs
+			FString TravelURL;
+
+			if (IsValid(PlayerController) && Sessions->GetResolvedConnectString(SessionName, TravelURL))
+			{
+				// Finally call the ClienTravel. If you want, you could print the TravelURL to see
+				// how it really looks like
+				PlayerController->ClientTravel(TravelURL, ETravelType::TRAVEL_Absolute);
+			}
+		}
+	}
+}
+
+void UAHGameInstance::OnDestroySessionComplete(FName SessionName, bool bWasSuccessful)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, FString::Printf(TEXT("OnDestroySessionComplete %s, %d"), *SessionName.ToString(), bWasSuccessful));
+
+	// Get the OnlineSubsystem we want to work with
+	IOnlineSubsystem* OnlineSub = IOnlineSubsystem::Get();
+	if (OnlineSub)
+	{
+		// Get the SessionInterface from the OnlineSubsystem
+		IOnlineSessionPtr Sessions = OnlineSub->GetSessionInterface();
+
+		if (Sessions.IsValid())
+		{
+			// Clear the Delegate
+			Sessions->ClearOnDestroySessionCompleteDelegate_Handle(OnDestroySessionCompleteDelegateHandle);
+
+			// If it was successful, we just load another level (could be a MainMenu!)
+			if (bWasSuccessful)
+			{
+				UGameplayStatics::OpenLevel(GetWorld(), MainMenuMapName, true);
+			}
+		}
+	}
+}
+
+
+void UAHGameInstance::HandleNetworkFailure(UWorld* World, UNetDriver* NetDriver, ENetworkFailure::Type FailureType, const FString& ErrorMessage)
+{
+	DisplayNetworkErrorMessage(ErrorMessage);
+}
+
+void UAHGameInstance::OnTravelFailure(UWorld* World, ETravelFailure::Type FailureType, const FString& ErrorMessage)
+{
+	DisplayNetworkErrorMessage(ErrorMessage);
+}
+
+void UAHGameInstance::DisplayNetworkErrorMessage(FString ErrorMessage)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Red, ErrorMessage);
 }
--- Source/AcidHouse/AHGameInstance.h
+++ Source/AcidHouse/AHGameInstance.h
@@ -1,19 +1,173 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Interfaces/OnlineSessionInterface.h"
 #include "AHGameInstance.generated.h"
 
-/**
- * 
- */
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnMatchFound, bool)
+
 UCLASS()
 class ACIDHOUSE_API UAHGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
 public:
+	UAHGameInstance();
+
+	FOnMatchFound OnMatchFound;
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
 	virtual bool ProcessConsoleExec(const TCHAR* Cmd, FOutputDevice& Ar, UObject* Executor) override;
+
+	virtual void Init() override;
+
+	virtual void Shutdown() override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Server Settings")
+	FName LobbyMapName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Server Settings")
+	FName MainMenuMapName;
+
+	UPROPERTY(Replicated, EditAnywhere, Category = "Server Settings")
+	FName ServerName;
+
+	UPROPERTY(Replicated, EditAnywhere, Category = "Server Settings")
+	uint32 MaxPlayers;
+
+#pragma region CreatingNetworkSession
+
+	/**
+	*	Function to host a game!
+	*
+	*	@Param		UserID			User that started the request
+	*	@Param		SessionName		Name of the Session
+	*	@Param		bIsLAN			Is this is LAN Game?
+	*	@Param		bIsPresence		"Is the Session to create a presence Session"
+	*	@Param		MaxNumPlayers	        Number of Maximum allowed players on this "Session" (Server)
+	*/
+	bool HostSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, bool bIsLAN, bool bIsPresence, int32 MaxNumPlayers);
+
+	/* Delegate called when session created */
+	FOnCreateSessionCompleteDelegate OnCreateSessionCompleteDelegate;
+
+	/* Delegate called when session started */
+	FOnStartSessionCompleteDelegate OnStartSessionCompleteDelegate;
+
+	TSharedPtr<class FOnlineSessionSettings> SessionSettings;
+
+	/** Handles to registered delegates for creating/starting a session */
+	FDelegateHandle OnCreateSessionCompleteDelegateHandle;
+	FDelegateHandle OnStartSessionCompleteDelegateHandle;
+
+	/**
+	*	Function fired when a session create request has completed
+	*
+	*	@param SessionName the name of the session this callback is for
+	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	*/
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+
+	/**
+	*	Function fired when a session start request has completed
+	*
+	*	@param SessionName the name of the session this callback is for
+	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	*/
+	void OnStartOnlineGameComplete(FName SessionName, bool bWasSuccessful);
+
+#pragma endregion CreatingNetworkSession
+
+#pragma region FindingNetworkSessions
+
+	/**
+	*	Find an online session
+	*
+	*	@param UserId user that initiated the request
+	*	@param bIsLAN are we searching LAN matches
+	*	@param bIsPresence are we searching presence sessions
+	*/
+	void FindSessions(TSharedPtr<const FUniqueNetId> UserId, bool bIsLAN, bool bIsPresence);
+
+	/** Delegate for searching for sessions */
+	FOnFindSessionsCompleteDelegate OnFindSessionsCompleteDelegate;
+
+	/** Handle to registered delegate for searching a session */
+	FDelegateHandle OnFindSessionsCompleteDelegateHandle;
+
+	TSharedPtr<class FOnlineSessionSearch> SessionSearch;
+
+	/**
+	*	Delegate fired when a session search query has completed
+	*
+	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	*/
+	void OnFindSessionsComplete(bool bWasSuccessful);
+
+#pragma endregion FindingNetworkSessions
+
+#pragma region JoiningNetworkSessions
+
+	/**
+	*	Joins a session via a search result
+	*
+	*	@param SessionName name of session
+	*	@param SearchResult Session to join
+	*
+	*	@return bool true if successful, false otherwise
+	*/
+	bool JoinFoundOnlineSession(TSharedPtr<const FUniqueNetId> UserId, FName SessionName, const FOnlineSessionSearchResult& SearchResult);
+
+	/** Delegate for joining a session */
+	FOnJoinSessionCompleteDelegate OnJoinSessionCompleteDelegate;
+
+	/** Handle to registered delegate for joining a session */
+	FDelegateHandle OnJoinSessionCompleteDelegateHandle;
+
+	/**
+	*	Delegate fired when a session join request has completed
+	*
+	*	@param SessionName the name of the session this callback is for
+	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	*/
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+#pragma endregion JoiningNetworkSessions
+
+#pragma region DestroyingASession
+	/** Delegate for destroying a session */
+	FOnDestroySessionCompleteDelegate OnDestroySessionCompleteDelegate;
+
+	/** Handle to registered delegate for destroying a session */
+	FDelegateHandle OnDestroySessionCompleteDelegateHandle;
+
+	/**
+	*	Delegate fired when a destroying an online session has completed
+	*
+	*	@param SessionName the name of the session this callback is for
+	*	@param bWasSuccessful true if the async action completed without error, false if there was an error
+	*/
+	virtual void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
+
+#pragma endregion DestroyingEndRegion
+
+#pragma region HandlingNetworkErrors
+
+	FDelegateHandle OnNetworkFailureEventHandle;
+
+	FDelegateHandle OnTravelFailureEventHandle;
+
+	
+	void HandleNetworkFailure(UWorld* World, UNetDriver* NetDriver, ENetworkFailure::Type FailureType, const FString& ErrorString);
+
+	UFUNCTION()
+		void OnTravelFailure(UWorld* World, ETravelFailure::Type FailureType, const FString& ErrorMessage);
+
+#pragma endregion HandlingNetworkErrors
+
+private:
+	void DisplayNetworkErrorMessage(FString ErrorMessage);
 };
